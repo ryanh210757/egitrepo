e53e8558fd3c3b08b441f8f9ca22afa8e5071ba9
ryanh210757 <kryanharris1@gmail.com>
2021-03-17 10:53:29 -0500

    new feature add

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("New Line is being added");
+		System.out.println("This is new feature");
 	}
 
 }
